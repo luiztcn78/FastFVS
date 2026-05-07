609b8820c326a4163336474ec920154a9f6c1190
luiztcn78 <luiztenorio789@gmail.com>
2026-05-07 19:40:06 -0300

    Feat: Tela ler qrcode

--- fastfvs_front/lib/view/pages/pagina_ler_qrcode.dart
+++ fastfvs_front/lib/view/pages/pagina_ler_qrcode.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
-class PaginaLerQrcode extends StatelessWidget{
+class PaginaLerQrcode extends StatelessWidget {
   const PaginaLerQrcode({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      color: const Color.fromARGB(255, 255, 255, 255),
+      child: Center(
+        child: Container(
+          width: 400,
+          height: 550,
+          color: const Color.fromARGB(255, 0, 0, 0),
+          child: const Center(
+            child: const Text(
+              "Ler QR Code",
+              style: TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
