3a96b8283a90d9c7d6b31e92d38a69a836961e50
luiztcn78 <luiztenorio789@gmail.com>
2026-05-07 22:20:39 -0300

    Feat: Tela de Configurações

--- fastfvs_front/lib/view/pages/pagina_configuracao.dart
+++ fastfvs_front/lib/view/pages/pagina_configuracao.dart
@@ -1,12 +1,133 @@
 import 'package:flutter/material.dart';
 
-class PaginaConfiguracao extends StatelessWidget{
+class PaginaConfiguracao extends StatelessWidget {
   const PaginaConfiguracao({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.amber,
+      color: const Color.fromARGB(255, 255, 255, 255),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Padding(
+              padding: EdgeInsets.only(left: 20, top: 20),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "DarkMode",
+                    style: TextStyle(
+                      color: Color.fromRGBO(3, 1, 0, 1),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Switch(
+                    value: true,
+                    onChanged: null,
+                    activeColor: Colors.white,
+                    activeTrackColor: Color.fromRGBO(3, 1, 0, 1),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            SizedBox(height: 40.0),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "      Perfil",
+                    style: TextStyle(
+                      color: Color.fromRGBO(3, 1, 0, 1),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  Icon(
+                    Icons.account_circle_outlined,
+                    size: 24.0,
+                    color: Color.fromRGBO(3, 1, 0, 1),
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "      Trocar de conta",
+                    style: TextStyle(
+                      color: Color.fromRGBO(3, 1, 0, 1),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  Icon(
+                    Icons.switch_account_outlined,
+                    size: 24.0,
+                    color: Color.fromRGBO(3, 1, 0, 1),
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "      Excluir conta",
+                    style: TextStyle(
+                      color: Color.fromRGBO(3, 1, 0, 1),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  Icon(
+                    Icons.delete_outline,
+                    size: 24.0,
+                    color: Color.fromRGBO(3, 1, 0, 1),
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "      Sair",
+                    style: TextStyle(
+                      color: Color.fromRGBO(255, 0, 0, 1),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  Icon(
+                    Icons.logout,
+                    size: 24.0,
+                    color: Color.fromRGBO(255, 0, 0, 1),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 40.0),
+          ],
+        ),
+      ),
     );
   }
 }
