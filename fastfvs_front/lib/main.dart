e244aa451a2dd417198446ff18b2af97a1f7eeac
Vinicius Gabriel <vinicius.gsbezerra@upe.br>
2026-05-14 20:40:21 -0300

    Add: pagina fvs

--- fastfvs_front/lib/main.dart
+++ fastfvs_front/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:fastfvs_front/config/theme_light.dart';
 import 'package:fastfvs_front/view/pages/pagina_configuracao.dart';
 import 'package:fastfvs_front/view/pages/pagina_ler_qrcode.dart';
 import 'package:fastfvs_front/view/pages/pagina_minhas_obras.dart';
-import 'package:fastfvs_front/view/pages/pagina_obra.dart';
 import 'package:flutter/material.dart';
 
 void main() {
---
+++ fastfvs_front/lib/view/widgets/fvs.dart
@@ -0,0 +1,65 @@
+import 'package:fastfvs_front/view/widgets/popup_status_fvs.dart';
+import 'package:flutter/material.dart';
+
+class Fvs extends StatefulWidget {
+  final String nome;
+  const Fvs({required this.nome, super.key});
+
+  @override
+  State<Fvs> createState() => FvsState();
+}
+
+
+class FvsState extends State<Fvs>{
+
+  Color status = Colors.grey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: InkWell(
+        onTap: () {
+          showDialog(
+            context: context,
+            builder: (context) => Dialog(
+              insetPadding: EdgeInsets.zero,
+              child: PopUpStatusFvs(statusSelecionado: (cor) {
+                setState(() => status = cor);
+              },
+                nomeFvs: widget.nome), // ← seu widget de popup
+            ),
+          );
+        },
+        child: Container(
+          width: MediaQuery.of(context).size.width*0.8,
+          height: MediaQuery.of(context).size.height*0.055,
+          decoration: BoxDecoration(
+            border: Border.all(color: Theme.of(context).colorScheme.primary),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(widget.nome),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: Container(
+                  width: 25,
+                  height: 25,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: status
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
