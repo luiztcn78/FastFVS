fcdf359896a090894a1f2c7f504c16a42dfea94b
Mwrcelo <marcelo.asmoura@upe.br>
2026-06-25 01:56:09 -0300

    UPD: Página de Carregamento, temporizador

--- fastfvs_front/lib/view/pages/pagina_carregamento.dart
+++ fastfvs_front/lib/view/pages/pagina_carregamento.dart
@@ -2,51 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:fastfvs_front/view/widgets/bolinhas_carregamento.dart';
 import 'package:fastfvs_front/view/pages/pagina_login.dart';
 
-class PaginaCarregamento extends StatelessWidget {
+class PaginaCarregamento extends StatefulWidget {
   const PaginaCarregamento({super.key});
 
   @override
-    Widget build(BuildContext context) {
+  State<PaginaCarregamento> createState() => _PaginaCarregamentoState();
+}
+
+class _PaginaCarregamentoState extends State<PaginaCarregamento> {
+  @override
+  void initState() {
+    super.initState();
+    _iniciarCarregamento();
+  }
+
+  Future<void> _iniciarCarregamento() async {
+
+    await Future.delayed(const Duration(seconds: 10));
+
+    if (mounted) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const PaginaLogin()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start, 
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                 const SizedBox(height: 140),
-                Image.asset('assets/images/logo_fastfvs.png',
-                width: 250,),
-
+                const SizedBox(height: 140),
+                Image.asset(
+                  'assets/images/logo_fastfvs.png',
+                  width: 250,
+                ),
                 const SizedBox(height: 30),
                 BolinhasCarregamento(),
-                const SizedBox(height: 30),
-                
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const PaginaLogin()),
-                  );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                  child: Text(
-                    'Entrar',
-                    style: TextStyle(fontSize: 18, 
-                    color:Theme.of(context).colorScheme.secondary),
-                  ),
-                ),
               ],
             ),
-          ),         
+          ),
         ],
-      )
-      );
-    }   
+      ),
+    );
   }
+}
